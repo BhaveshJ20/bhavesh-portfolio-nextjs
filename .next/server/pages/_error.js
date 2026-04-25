09c0dd4a7175821722d9d51cee2c14dd21378bfa
bhavesh <bjariwala@plusonex.com>
2026-04-25 09:56:24 +0530

    updated next.config

--- .next/server/pages/_error.js
+++ .next/server/pages/_error.js
@@ -3,6 +3,6 @@ R.c("server/chunks/ssr/node_modules_next_dist_0vykjmc._.js")
 R.c("server/chunks/ssr/[root-of-the-server]__0mt_2zq._.js")
 R.c("server/chunks/ssr/[root-of-the-server]__0i0fg9j._.js")
 R.c("server/chunks/ssr/[root-of-the-server]__0gyhdx0._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0a8if7e._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__0drxtc9._.js")
 R.m(7654)
 module.exports=R.m(7654).exports
